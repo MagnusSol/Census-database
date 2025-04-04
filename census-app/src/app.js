70670592af3381c93ee4c51ca3a7627398be4ee1
Magnus Solgaard <solgaard91@gmail.com>
2025-04-04 20:55:52 +0200

    changed port

--- census-app/src/app.js
+++ census-app/src/app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/participants', participantRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.DB_PORT;
 
 async function initializeApp() {
   try {
